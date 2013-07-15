b7ee1a28b703174bb08733861dbb8fb99da85882
Addison Luh <addison.luh@gmail.com>
2013-07-15 01:10:43 -0700

    Added new params to Wonder and Stage, with docs. Added and updated tests.

---
+++ tests/wonder_test.py
@@ -0,0 +1,16 @@
+
+import unittest
+
+import bonus
+import enum
+import wonder as wonder_lib
+
+class WonderTest(unittest.TestCase):
+  def testCreateWonder(self):
+    wonder = wonder_lib.Wonder('Boogie', [], enum.Resource.PAPYRUS)
+    self.assertIsNotNone(wonder)
+
+class StageTest(unittest.TestCase):
+  def testCreateStage(self):
+    stage = wonder_lib.Stage({enum.Resource.CLAY: 2}, bonus.PointBonus(3))
+    self.assertIsNotNone(stage)
