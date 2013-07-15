d631e462ee53f84232191f53cd0891b3d3583a64
Addison Luh <addison.luh@gmail.com>
2013-07-15 02:11:26 -0700

    Removed duplicate code from a merge.

--- wonder.py
+++ wonder.py
@@ -21,15 +21,6 @@ class Wonder(object):
     return '<' + str(self) + '>'
 
 
-  def __eq__(self, other):
-    return self.__dict__ == other.__dict__
-
-  def __str__(self):
-    return self.__class__.__name__ + ': ' + str(self.__dict__)
-
-  def __repr__(self):
-    return '<' + str(self) + '>'
-
 class Stage(object):
   def __init__(self, cost, bonus):
     """
