327478d1efe498de4babbc77b677f1bbbd7163d6
Addison Luh <addison.luh@gmail.com>
2013-07-21 21:24:52 -0700

    Added docs for game methods.

--- controller.py
+++ controller.py
@@ -13,33 +13,44 @@ class Game(object):
     Args:
       asset_file: Path to asset file.
     """
-    self.resetGame()
+    self.reset()
     self._loadAssets(asset_file)
 
-  def resetGame(self):
+  def reset(self):
     self.players = []
     self.all_cards = []
     self.current_cards = []
     self.wonders = []
 
   def addPlayer(self, name):
+    """Add a player to the game. Order matters, so add in clockwise order.
+
+    Args:
+      name: Name of the player.
+
+    Returns:
+      Index position of the newly added player.
+    """
     player = player_lib.Player(name)
     self.players.append(player)
     return len(self.players) - 1
 
+  def setupGame(self):
+    """Set up the game after players have been added."""
+    self._setupPlayers()
+    self._pruneAssets()
+
   def setPlayerWonder(self, index, wonder_name):
+    """Set the player's wonder board.
+
+    Args:
+      index: Index position of the player.
+      wonder_name: Name of the wonder for the player.
+    """
     wonder = self._getWonder(wonder_name)
     if not wonder:
       raise exception.GameException('Invalid wonder name!')
     self.players[index].wonder = wonder
-
-  def setupGame(self):
-    """
-    Args:
-      players: List of players, in clockwise order.
-    """
-    self._setupPlayers()
-    self._pruneAssets()
 
   def _setupPlayers(self):
     utils.updatePlayerRelations(self.players)
